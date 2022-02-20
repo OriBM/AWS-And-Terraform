ebcc30070f9919e0f93ecfe27002839ffae91489
oribm <oribenmaor@gmail.com>
2022-02-20 18:45:51 +0200

    i just realized i forgot to config the sec group :|. anyway.. there it is

--- Homework1/main.tf
+++ Homework1/main.tf
@@ -93,6 +93,25 @@ resource "aws_instance" "secondwhiskey" {
 
 resource "aws_security_group" "allow_ssh" {
   name        = "nginx"
-  description = "allow ssh to nginx"
+  description = "Allow ports for nginx"
   vpc_id      = aws_default_vpc.default.id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
