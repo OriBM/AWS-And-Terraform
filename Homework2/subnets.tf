d651255b2acd88354f1c98d90a63923e4706c180
oribm <oribenmaor@gmail.com>
2022-03-06 17:26:51 +0200

    added public subnet + userdata

--- Homework2/subnets.tf
+++ Homework2/subnets.tf
@@ -8,3 +8,13 @@ resource "aws_subnet" "private_subnet" {
   tags = merge(local.common_tags, { Name = "private-subnet" })
 }
 
+#public subnet
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count = var.public_subnet_count
+  cidr_block              = local.public_subnets_addresses[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = merge(local.common_tags, { Name = "public-subnet" })
+}
