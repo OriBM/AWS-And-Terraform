7022fc55151717b73fb53dd593d57ff534eca44b
oribm <oribenmaor@gmail.com>
2022-03-06 17:29:58 +0200

    mission complete

---
+++ Homework2/variables.tf
@@ -0,0 +1,44 @@
+variable "private_key_path" {
+}
+variable "key_name" {
+}
+
+variable "aws_region" {
+}
+variable "instance_type" {
+}
+
+variable network_address_space {
+}
+variable "subnet1_address_space" {
+}
+variable "subnet2_address_space" {
+}
+
+variable "number_of_nginx_instances" {
+}
+variable "public_subnet_count" {
+}
+variable "private_subnet_count" {
+}
+variable "ubuntu_account_number" {
+}
+
+variable "owner_tag" {
+}
+variable "purpose_tag" {
+}
+
+variable "name_tag" {
+}
+locals {
+{
+  }
+  public_subnets_addresses = [
+  ]
+  private_subnets_addresses = [
+  ]
+
+
+
+}
